be535652a37a669f7d9c7b568b8668539118322f
gao_xianglong@sina.com <ll.520GG>
2017-07-28 18:26:17 +0800

    调整动态向spring的ioc容器中注册bean实例实现相关代码，不写临时文件

--- src/main/java/com/sharksharding/resources/register/bean/RegisterDataSource.java
+++ src/main/java/com/sharksharding/resources/register/bean/RegisterDataSource.java
@@ -15,9 +15,6 @@
  */
 package com.sharksharding.resources.register.bean;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
@@ -25,13 +22,10 @@ import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.jdbc.core.JdbcTemplate;
-
 import com.sharksharding.core.shard.GetJdbcTemplate;
-import com.sharksharding.exception.RegisterBeanException;
 import com.sharksharding.exception.SharkRuntimeException;
-import com.sharksharding.util.TmpManager;
 
 /**
  * 动态向spring的ioc容器中注册bean实例实现
@@ -83,46 +77,33 @@ public class RegisterDataSource implements RegisterBean {
 	 * @author gaoxianglong
 	 * 
 	 * @param nodePathValue
-	 *            zookeeper注册中心的节点value
+	 *            value 从配置中心订阅的配置信息
 	 * 
 	 * @param resourceType
 	 *            注册中心类型
 	 * 
 	 * @return void
 	 */
-	public static void register(String nodePathValue, String resourceType) {
-		if (null == aContext)
+	public static void register(String value, String resourceType) {
+		if (null == aContext || null == value)
 			return;
-		final String tmpdir = TmpManager.createTmp();
-		logger.debug("tmpdir-->" + tmpdir);
-		try (BufferedWriter out = new BufferedWriter(new FileWriter(tmpdir))) {
-			if (null != nodePathValue) {
-				out.write(nodePathValue);
-				out.flush();
-				FileSystemResource resource = new FileSystemResource(tmpdir);
-				ConfigurableApplicationContext cfgContext = (ConfigurableApplicationContext) aContext;
-				DefaultListableBeanFactory beanfactory = (DefaultListableBeanFactory) cfgContext.getBeanFactory();
-				/*
-				 * 将配置中心获取的配置信息与当前上下文中的ioc容器进行合并,不需要手动移除之前的bean,
-				 * 调用loadBeanDefinitions()方法时会进行自动移除
-				 */
-				new XmlBeanDefinitionReader(beanfactory).loadBeanDefinitions(resource);
-				final String defaultBeanName = "jdbcTemplate";
-				String[] beanNames = beanfactory.getBeanDefinitionNames();
-				for (String beanName : beanNames) {
-					/* 替换上下文中缺省beanName为jdbcTemplate的JdbcTemplate的引用 */
-					if (defaultBeanName.equals(beanName)) {
-						GetJdbcTemplate.setJdbcTemplate((JdbcTemplate) beanfactory.getBean(defaultBeanName));
-					} else {
-						/* 实例化所有所有未实例化的bean */
-						beanfactory.getBean(beanName);
-					}
-				}
+		ConfigurableApplicationContext cfgContext = (ConfigurableApplicationContext) aContext;
+		DefaultListableBeanFactory beanfactory = (DefaultListableBeanFactory) cfgContext.getBeanFactory();
+		/*
+		 * 将配置中心获取的配置信息与当前上下文中的ioc容器进行合并,不需要手动移除之前的bean,
+		 * 调用loadBeanDefinitions()方法时会进行自动移除
+		 */
+		new XmlBeanDefinitionReader(beanfactory).loadBeanDefinitions(new ByteArrayResource(value.getBytes()));
+		final String defaultBeanName = "jdbcTemplate";
+		String[] beanNames = beanfactory.getBeanDefinitionNames();
+		for (String beanName : beanNames) {
+			/* 替换上下文中缺省beanName为jdbcTemplate的JdbcTemplate的引用 */
+			if (defaultBeanName.equals(beanName)) {
+				GetJdbcTemplate.setJdbcTemplate((JdbcTemplate) beanfactory.getBean(defaultBeanName));
+			} else {
+				/* 实例化所有所有未实例化的bean */
+				beanfactory.getBean(beanName);
 			}
-		} catch (Exception e) {
-			throw new RegisterBeanException(e.toString());
-		} finally {
-			TmpManager.deleteTmp(tmpdir);
 		}
 	}
 }
